c175f5f6ecdf572a2f88ff858316618c6260397d
VolanDeMor1 <volandemort2020@gmail.com>
2023-05-17 21:02:25 +0300

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.21"
+    id("java")
+    id("maven-publish")
 }
 
-group = "lol.asgard"
-version = "1.2"
+val groupVal = "lol.asgard"
+val artifactVal = "EscapeTimeApi"
+val versionVal = "1.2"
+
+group = groupVal
+version = versionVal
 
 repositories {
     mavenCentral()
@@ -18,6 +24,18 @@ dependencies {
     compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT")
     compileOnly("net.kyori:adventure-text-minimessage:4.13.0")
     implementation("com.github.jitpack:gradle-simple:master-SNAPSHOT")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = groupId
+            artifactId = artifactVal
+            version = versionVal
+
+            from(components["java"])
+        }
+    }
 }
 
 tasks.compileJava {
