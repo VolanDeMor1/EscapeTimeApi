66f9dabe4cf98bff7c249b80dcf8f1adefe00eea
VolanDeMor1 <volandemort2020@gmail.com>
2023-05-17 15:47:49 +0300

    fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,23 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.8.21"
 }
 
 group = "lol.asgard"
-version = "1.1"
+version = "1.2"
 
 repositories {
     mavenCentral()
     maven("https://repo.purpurmc.org/snapshots")
+    maven("https://jitpack.io")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     compileOnly("org.purpurmc.purpur:purpur-api:1.19.4-R0.1-SNAPSHOT")
     compileOnly("net.kyori:adventure-text-minimessage:4.13.0")
+    implementation("com.github.jitpack:gradle-simple:master-SNAPSHOT")
 }
 
 tasks.compileJava {
@@ -22,6 +26,10 @@ tasks.compileJava {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
 }
 
 kotlin {
