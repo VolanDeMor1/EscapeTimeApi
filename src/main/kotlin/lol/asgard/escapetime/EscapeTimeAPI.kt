97e38b533adbd28124bd78f78ccbff2b6471088e
VolanDeMor1 <volandemort2020@gmail.com>
2023-05-21 13:05:21 +0300

    1.3.6

--- src/main/kotlin/lol/asgard/escapetime/EscapeTimeAPI.kt
+++ src/main/kotlin/lol/asgard/escapetime/EscapeTimeAPI.kt
@@ -3,8 +3,10 @@ package lol.asgard.escapetime
 import lol.asgard.escapetime.api.`object`.MapType
 import lol.asgard.escapetime.api.`object`.game.Game
 import lol.asgard.escapetime.api.`object`.gamer.Gamer
+import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
+import org.bukkit.plugin.Plugin
 
 interface EscapeTimeAPI {
 
@@ -14,5 +16,14 @@ interface EscapeTimeAPI {
     fun join(gamer: Gamer, game: Game)
     fun gamerOf(player: Player): Gamer
     fun gamerOf(player: OfflinePlayer): Gamer?
+
+    companion object {
+        @JvmStatic
+        fun get(): EscapeTimeAPI {
+            val pl: Plugin? = Bukkit.getPluginManager().getPlugin("EscapeTime")
+            if (pl != null) return (pl as EscapeTime).api()
+            else throw NullPointerException("Плагин EscapeTime не обнаружен (см. подробнее https://github.com/VolanDeMor1/EscapeTimeApi#errors)")
+        }
+    }
 
 }
